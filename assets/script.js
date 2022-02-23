8b7bf0c5ab5c42490d5a7f6f0374201541d67469
SeanShin0821 <cshin4@gmu.edu>
2022-02-23 11:54:23 -0500

    JS update

--- assets/script.js
+++ assets/script.js
@@ -24,19 +24,19 @@ function generatePassword() {
   // First prompt asking for users password length
   while (true) {
     passwordLength = parseInt(prompt("Choose the length of your password. This needs to be between 8 - 128"));
-      // least 8 no more than 128 
+      // At least 8 no more than 128 
     if (passwordLength >= 8 && passwordLength <= 128) {
       //break function stops the unending loop 
       break;
     }
-  // Else, return this alert and loop back to the start so they can choose a valid number
   alert("Please select a number between 8 - 128");
 }
 // After user chooses a number it will proceed by asking which elements to add 
-symbols = confirm("Press'OK' to add special characters");
+symbols = confirm("Press 'OK' to add special characters");
 numbers = confirm("Press 'OK' to add numbers");
 upperchar = confirm("Press 'OK' to add uppercase letters");
 lowerchar = confirm("Press 'OK' to add lowercase letters");
+
 
 
 // Function for options of creating an element 
@@ -51,36 +51,50 @@ lowerchar = confirm("Press 'OK' to add lowercase letters");
     passwordChar = lower;
     // Combination of two elements using AND function if one is true other is too 
   } else if (symbols && numbers) {
-    passwordChar = sym += num;
+    passwordChar = sym + num;
   } else if (symbols && upperchar) {
-    passwordChar = sym += upper;
-  } else if (symbols && lowercase) {
-    passwordChar = sym += lower;
+    passwordChar = sym + upper;
+  } else if (symbols && lowerchar) {
+    passwordChar = sym + lower;
   } else if (numbers && upperchar) {
-    passwordChar = num += upper;
+    passwordChar = num + upper;
   } else if (numbers && lowerchar) {
-    passwordChar = num += lower;
+    passwordChar = num + lower;
   } else if (upperchar && lowerchar) {
-    passwordChar = upper += lower;
+    passwordChar = upper + lower;
     // Combination of three elements using AND function if one is true other two.
   } else if (symbols && numbers && upperchar) {
-    passwordChar = sym += num += upper;
+    passwordChar = sym + num + upper;
   } else if (symbols && numbers && lowerchar) {
-    passwordChar = sym += num += lower;
+    passwordChar = sym + num + lower;
   } else if (symbols && upperchar && lowerchar) {
-    passwordChar = sym += upper += lower;
+    passwordChar = sym + upper + lower;
   } else if (lowerchar && numbers && upperchar) {
-    passwordChar = lower += num += upper;
+    passwordChar = lower + num + upper;
     // Combination of four elements using AND function of one is true other three is. 
   } else if (symbols && numbers && upperchar && lowerchar) {
-    passwordChar = sym += num += upper += lower;
+    passwordChar = sym + num + upper + lower;
   } else if (!symbols && !numbers && !upperchar && !lowerchar)
     alert("At least choose a single material")
 
-    // For loop to select random characters from the criteria strings
-    for (var i = 0; i < passwordLength; i++) {
-      var password = passwordChar[Math.floor(Math.random() * passwordChar.length)]
-    }
+    // FOr loop 
+  for (var i = 0; i < passwordLength; i++) {
+      var password = passwordChar[Math.floor(Math.random() * password.length)]
+  }
   
-    return (password);
+  return (password);
+  }
+
+  //original document from here 
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
