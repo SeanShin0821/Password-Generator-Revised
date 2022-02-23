be06f69c9d466dad6e74a44c9c344f19bbb0281f
SeanShin0821 <cshin4@gmu.edu>
2022-02-22 21:37:32 -0500

    index update

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,20 @@
 // Initial generation for button variable 
 var generateBtn = document.querySelector("#generate");
 
+// Inputs (char, num, special )
+var passwordLength;
+var symbols;
+var numbers;
+var upperchar;
+var lowerchar;
 
+// Source for password generation 
+var sym = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "-", "_", "(", ")", "~", "`"];
+var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
-
-
-
+//function for generating password 
 
 
 
